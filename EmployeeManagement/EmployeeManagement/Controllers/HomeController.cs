296f6ae6a59777528196ff1b1f06a14398bbea97
asyrafahmad <are.syraf9764@gmail.com>
2019-10-09 17:03:33 +0800

    Initial commit (Part1-32)

---
+++ EmployeeManagement/EmployeeManagement/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using EmployeeManagement.Models;
+using EmployeeManagement.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        //readonly = good practice to prevents accidently assigning another value to the this private field(_employeeRepository) inside another method within this HomeController
+        private readonly IEmployeeRepository _employeeRepository;
+
+        // define constuctor parameter for HomeController
+        public HomeController(IEmployeeRepository employeeRepository)                       // its called constructor injection because we inject IEmployeeRepository into HomeController
+        {
+            _employeeRepository = employeeRepository;
+        }
+        
+        public ViewResult Index()
+        {
+            var model = _employeeRepository.GetAllEmployee();
+            return View(model);
+        }
+
+        public ViewResult Details()
+        {
+            HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
+            {
+                Employee = _employeeRepository.GetEmployee(2),
+                PageTitle = "Employee Details" 
+            };
+
+            return View(homeDetailsViewModel);
+        }
+    }
+}
